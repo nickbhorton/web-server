044d01bbb776217cdb08e090f4bdda24ccfdf26c
Nick Horton <nickkbhorton@gmail.com>
2024-02-27 08:19:27 -0600

    trimming request

--- server.cc
+++ server.cc
@@ -12,6 +12,20 @@ void die(const std::string& msg) {
     std::exit(EXIT_FAILURE);
 }
 
+// https://stackoverflow.com/questions/1798112/removing-leading-and-trailing-spaces-from-a-string
+std::string trim(const std::string& str,
+                 const std::string& whitespace = " \t")
+{
+    const auto strBegin = str.find_first_not_of(whitespace);
+    if (strBegin == std::string::npos)
+        return ""; // no content
+
+    const auto strEnd = str.find_last_not_of(whitespace);
+    const auto strRange = strEnd - strBegin + 1;
+
+    return str.substr(strBegin, strRange);
+}
+
 int main(int argc, char** argv) {
     if (argc != 3) {
         die("2 cmd args reqired | <address> <port>");
@@ -60,13 +74,13 @@ int main(int argc, char** argv) {
         size_t found = request_line.find(":");
         if (found != std::string::npos) {
             std::cout << "lhs:\n"; 
-            std::cout << request_line.substr(0, found) << "\n"; 
+            std::cout << "\t" << trim(request_line.substr(0, found)) << "\n"; 
             std::cout << "rhs:\n"; 
-            std::cout << request_line.substr(found) << "\n"; 
+            std::cout << "\t"<< trim(request_line.substr(found + 1)) << "\n"; 
         }
         else {
             std::cout << "color character was no found in field" << "\n";
-            std::cout << request_line << "\n";
+            std::cout << "\t"<< trim(request_line) << "\n";
         }
     }
     
