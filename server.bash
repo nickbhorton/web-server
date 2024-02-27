95836eee748b940f71ad6c2a8a902f16b2df6afc
Nick Horton <nickkbhorton@gmail.com>
2024-02-27 14:25:57 -0600

    added documentatuon

--- server.bash
+++ server.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 server_address="10.0.0.69"
 port=8080
+# this was used in the server side to use port 8080 instead
+# https://serverfault.com/questions/112795/how-to-run-a-server-on-port-80-as-a-normal-user-on-linux
 
 ./server $server_address $port
 exit 0
